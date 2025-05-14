0bca0dbc5b97ab12f46fb1095576255d063383ce
olucasfracaro <88164818+olucasfracaro@users.noreply.github.com>
2025-05-14 16:44:04 -0300

    adição das queries no tabelas.sql

---
+++ tabelas.sql
@@ -0,0 +1,25 @@
+CREATE TABLE contas (
+  cpf VARCHAR(14) PRIMARY KEY,
+  nome VARCHAR(80) NOT NULL,
+  email VARCHAR(80) NOT NULL,
+  senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE manifestacoes (
+  cpf VARCHAR(14),
+  febre BOOLEAN,
+  fadiga BOOLEAN,
+  dorCorpo BOOLEAN,
+  dorGarganta BOOLEAN,
+  tosse BOOLEAN,
+  espirros BOOLEAN,
+  diarreia BOOLEAN,
+  nausea BOOLEAN,
+  vomitos BOOLEAN,
+  dorAbdominal BOOLEAN,
+  faltaApetite BOOLEAN,
+  malEstar BOOLEAN,
+  desidratacao BOOLEAN,
+
+  FOREIGN KEY (cpf) REFERENCES contas(cpf)
+)
